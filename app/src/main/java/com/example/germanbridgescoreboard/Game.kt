5e0ddaaf774957d3836f642401ba8b8a1710f9df
Daniel <danlooijj17102003@gmail.com>
2023-10-31 16:50:53 +0800

    prepare Game class

--- app/src/main/java/com/example/germanbridgescoreboard/Game.kt
+++ app/src/main/java/com/example/germanbridgescoreboard/Game.kt
@@ -1,4 +1,7 @@
 package com.example.germanbridgescoreboard
 
-class Game {
+class Game (var playerCount: Int){
+    var playerName: ArrayList<String> = arrayListOf()
+    var players: ArrayList<Player> = arrayListOf()
+    var rounds: Int = Math.floor(52.0/playerCount).toInt()
 }
