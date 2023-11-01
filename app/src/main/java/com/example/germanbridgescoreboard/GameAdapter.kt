d76a8bce588db747bdc3a160528eb9af9d1b14bd
Daniel <danlooijj17102003@gmail.com>
2023-11-01 17:19:26 +0800

    experimenting with game init

--- app/src/main/java/com/example/germanbridgescoreboard/GameAdapter.kt
+++ app/src/main/java/com/example/germanbridgescoreboard/GameAdapter.kt
@@ -1,29 +1,37 @@
 package com.example.germanbridgescoreboard
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputLayout
 
 class GameAdapter(var playerCount: Int) : RecyclerView.Adapter<GameAdapter.ViewHolder>() {
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
+        val textInputLayout: TextInputLayout
 
         init{
-            textView = view.findViewById(R.id.textView)
+            textView = view.findViewById(R.id.player_num)
+            textInputLayout = view.findViewById(R.id.input_player_name)
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val playerInputView = inflater.inflate(R.layout.item_game_init, parent, false)
+        return ViewHolder(playerInputView)
     }
 
     override fun onBindViewHolder(holder: GameAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val textView = holder.textView
+        textView.append(" " + (position + 1))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return playerCount
     }
 }
---
+++ app/src/main/java/com/example/germanbridgescoreboard/GameInit.kt
@@ -0,0 +1,13 @@
+package com.example.germanbridgescoreboard
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class GameInit(numPlayers: Int) : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        val rvPlayers = findViewById<View>(R.id.input_player_name) as RecyclerView
+    }
+}
