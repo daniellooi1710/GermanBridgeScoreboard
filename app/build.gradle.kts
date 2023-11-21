24107433c7ba8d597ad8573f3964bac952a2c11b
daniel <danlooijj17102003@gmail.com>
2023-11-21 11:52:06 +0800

    v1.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.1.7"
+        versionName = "1.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         javaCompileOptions {
